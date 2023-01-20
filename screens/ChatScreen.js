38c90a9c34d79ff38e03cac305fbfacf14c30d5b
root27 <oguzhaneee@gmail.com>
2023-01-20 19:52:49 +0300

    screens updated

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,85 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useState,useEffect,useCallback} from 'react'
+import { GiftedChat } from "react-native-gifted-chat";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { collection,addDoc,orderBy,query,onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { useLayoutEffect } from "react";
 
-const ChatScreen = () => {
+
+
+const ChatScreen = ({route}) => {
+
+  const [messages, setMessages] = useState([]);
+
+ 
+  useEffect(() => {
+    const collectionref = collection(db, "messages");
+
+    const q = query(collectionref, orderBy("createdAt", "desc"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setMessages(
+        querySnapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user
+        }))
+      )
+    })
+
+    return unsubscribe;
+  }, [])
+
+
+
+
+
+
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+
+    const { _id, createdAt, text, user } = messages[0];
+
+    addDoc(collection(db, "messages"), {
+      _id,
+      createdAt,
+      text,
+      user
+    })
+
+
+
+
+
+  }, [])
+
+    
+
+  
   return (
-    <View>
-      <Text>ChatScreen</Text>
+    <View style={{flex: 1}}>
+    <GiftedChat messages={messages} onSend={messages => {
+      onSend(messages)
+    }} user={{
+      _id: route.params.user.uid,
+      name: route.params.user.displayName
+    }}
+    
+    messagesContainerStyle= {{
+      backgroundColor: "white"
+    }}
+    
+    />
+     {
+      Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
+   }
     </View>
+
+    
+
   )
 }
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,93 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity} from 'react-native'
+import React,{useState} from 'react'
+import { useNavigation } from "@react-navigation/native"
+import {auth} from "../firebase"
+import { signInAnonymously, updateProfile } from "firebase/auth"
 
 const LoginScreen = () => {
+
+    const [name, setName] = useState("")
+
+    const navigation = useNavigation();
+
+    const [user, setUser] = useState(null);
+
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+        <View style={styles.circle} />
+        <View style={{marginTop: 64}}>
+            <Image source={require("../assets/chat.jpg")} style={{width: 100, height: 100, alignSelf: "center"}} />
+        </View>
+        <View style={{marginHorizontal: 32}}>
+            <Text style={{fontSize: 20, fontWeight: "500", color: "#514e5a",
+            marginTop: 20}}>Username
+            </Text>
+            <TextInput style={styles.input} placeholder="Enter your username" onChangeText={(text) => setName(text)} />
+      
+            <View style={{alignItems: "flex-end", marginTop: 64}}>
+                <TouchableOpacity onPress={
+                    () => {
+
+                        signInAnonymously(auth).then((userCredential) => {
+                            // Signed in..
+                            
+
+                            updateProfile(auth.currentUser, {
+                                displayName: name
+                                }).then(() => {
+                                // Update successful
+                                
+                                setUser(auth.currentUser);
+                                }).catch((error) => {
+                                // An error occurred
+                                console.log(error);
+                                });
+
+                            
+
+
+
+                            // ...
+                            }).catch((error) => {
+                            const errorCode = error.code;
+                            });
+
+                        navigation.navigate("Chat",{user: auth.currentUser
+                        })
+                    }
+                } style={{width: 200, height: 50, backgroundColor: "#9075e3", borderRadius: 30, alignItems: "center", justifyContent: "center"}}>
+                    <Text style={{color: "#fff", fontWeight: "500"}}>Next</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:"#f4f5f7"
+    },
+    circle: {
+        width: 500,
+        height: 500,
+        borderRadius: 500/2,
+        backgroundColor: "#fff",
+        position: "absolute",
+        left: -120,
+        top: -20
+    },
+    input:{
+        marginTop: 32,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "#bab7c3",
+        height: 50,
+        borderRadius: 30,
+        paddingHorizontal: 16,
+    }
+
+})
